00fdafe07bdb897a9485083e819c2625237c6547
Gizmo-30 <anvaradizov80@gmail.com>
2021-12-10 12:55:08 +0500

    first

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 const userInfo = {}
 
-var a = 0
-
 for (let i = 1; i <= 10; i++) {
     do {
         var userName = prompt('Введите имя ?')
@@ -10,15 +8,16 @@ for (let i = 1; i <= 10; i++) {
         var userAge = +prompt('Введите возраст ?')
     } while (isNaN(userAge));
     
-    userInfo[a = a + 1] = {
+    userInfo[i] = {
         name: userName,
         age: userAge
     }
 
-    for (const key in userInfo) {
-        for (const newkey in userInfo[key]) {
-            console.log(`Ползователь - ${key} \n Имя: ${userInfo[key].name} \n Возраст: ${userInfo[key].age}`);
-        }
+
+}
+for (const key in userInfo) {
+    for (const newkey in userInfo[key]) {
+        console.log(`Ползователь ${key} \n Имя: ${userInfo[key].name} \n Возраст: ${userInfo[key].age}`);
     }
 }
 
